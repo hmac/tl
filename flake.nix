ef756e6c3bd8cde2d8b6263365a8ba809da56875
Harry Maclean <harryjmaclean@googlemail.com>
2023-06-10 18:26:31 +0900

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "tl";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        rust = rust-overlay.packages.${system}.rust;
+      in
+      {
+        devShells.default = pkgs.mkShell
+          {
+            buildInputs = with pkgs; [
+              rust
+              rust-analyzer
+              libiconv
+            ];
+            # ++ (lib.optionals stdenv.isDarwin [
+            #   darwin.apple_sdk.frameworks.CoreServices
+            #   darwin.apple_sdk.frameworks.Security
+            #   darwin.apple_sdk.frameworks.SystemConfiguration
+            # ]);
+          };
+      });
+}
