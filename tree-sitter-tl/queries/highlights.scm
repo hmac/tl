d7c75f6b429b3b9c26d300f09f4b0bdbd93a3e31
Harry Maclean <harry@hmac.dev>
2024-05-10 16:47:47 +0100

    Update highlights for type decls

--- tree-sitter-tl/queries/highlights.scm
+++ tree-sitter-tl/queries/highlights.scm
@@ -10,11 +10,16 @@
 (func_decl
   name: (l_ident) @function)
 
+(type_decl
+  name: (u_ident) @type)
+(type_decl_ctor name: (u_ident) @type.enum.variant)
+
 (type_name) @type
-(var_type) @type
+(var_type) @type.parameter
 (app_type) @type
 (func_type) @type
 
 (ctor) @constructor
 
 (comment) @comment
+
